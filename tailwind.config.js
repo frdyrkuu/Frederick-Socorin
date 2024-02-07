fde2eb332044358bbd93f21fc9f6e32e302721be
Frdyrkuu <socorinfrederick@gmail.com>
2024-02-08 03:25:40 +0800

    achievement unlocked

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "*.html", "./admin/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [
     require('tailwind-scrollbar-hide')
