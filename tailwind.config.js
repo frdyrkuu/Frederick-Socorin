8ad86d3843e01bdbeb122a258033e38e2b4c0b1d
Frdyrkuu <socorinfrederick@gmail.com>
2024-02-26 21:32:56 +0800

    clone weblist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "*.html", "./others/*.html", "./samples/laufey/*.html" ],
+  content: ["./src/**/*.{html,js}", "*.html", "./others/*.html", "./samples/laufey/*.html", "./samples/*.html" ],
   theme: {
     extend: {
       keyframes: {
